cdc4f15d6e587d9d67374e254fc91b551f7882fe
oN0V41S <129956152+oN0V41S@users.noreply.github.com>
2024-03-25 04:50:52 +0000

    Add notebooks colab

---
+++ 1-estruturasCondicionaisEmML/exemplos.py
@@ -0,0 +1,16 @@
+# Estrutura de Condicional Simples
+soma = 3
+if soma > 0:
+  print("maior do que zero")
+else:
+  print("número menor que zero")
+
+# Estrutura de Condicional aninhada
+soma = -2
+
+if soma == 0:
+  print("Soma igual a zero")
+elif soma > 0:
+  print("soma é maior que zero")
+else:
+  print("soma é menor que zero")
---
+++ 2-tiposDeVariaveisEmML/exemplos.py
@@ -0,0 +1,7 @@
+# valores complexos
+a = 5+2j
+b = 20+6j
+
+print(type(a))
+print(type(b))
+print(complex(2,5))
